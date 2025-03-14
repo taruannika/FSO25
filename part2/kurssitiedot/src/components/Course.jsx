058b50b635ea78c1d8eb084351e55eceff9c79d5
HamalainenTaru <thamalainen1990@gmail.com>
2025-03-14 18:48:48 +0200

    part2 task 2.5

---
+++ part2/kurssitiedot/src/components/Course.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+
+const Header = ({ name }) => {
+  return <h1>{name}</h1>;
+};
+
+const Content = ({ parts }) => {
+  return (
+    <div>
+      {parts.map((part) => (
+        <Part key={part.id} part={part} />
+      ))}
+    </div>
+  );
+};
+
+const Part = ({ part }) => {
+  const { name, exercises } = part;
+  return (
+    <p>
+      {name} {exercises}
+    </p>
+  );
+};
+
+const Total = ({ parts }) => {
+  const total = parts.reduce((sum, part) => sum + part.exercises, 0);
+  return <h4>Total of {total} exercises</h4>;
+};
+const Course = ({ course }) => {
+  return (
+    <div>
+      <Header name={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </div>
+  );
+};
+
+export default Course;
